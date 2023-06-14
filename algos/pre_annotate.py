f085a72c32c24ed4402ebe2671516bebafd0f0be
LexaNagiBator228 <doctothenord@gmail.com>
2023-06-14 17:54:48 +0900

    rm tensorflow

--- algos/pre_annotate.py
+++ algos/pre_annotate.py
@@ -1,25 +1,16 @@
 
 import os
-import tensorflow as tf
-
-
-
 import numpy as np
 
-from . import util
 import glob
 import math
 import json
 
-util.config_gpu()
 
 
 RESAMPLE_NUM = 10
 
-model_file = "./algos/models/deep_annotation_inference.h5"
 
-rotation_model = tf.keras.models.load_model(model_file)
-rotation_model.summary()
 
 NUM_POINT=512
 
@@ -32,17 +23,7 @@ def sample_one_obj(points, num):
         return points[idx[0:num]]
 
 def predict_yaw(points):
-    points = np.array(points).reshape((-1,3))
-    input_data = np.stack([x for x in map(lambda x: sample_one_obj(points, NUM_POINT), range(RESAMPLE_NUM))], axis=0)
-    pred_val = rotation_model.predict(input_data)
-    pred_cls = np.argmax(pred_val, axis=-1)
-    print(pred_cls)
-    
-    ret = (pred_cls[0]*3+1.5)*np.pi/180.
-    ret =[0,0,ret]
-    print(ret)
-
-    return ret
+    return [0,0,1]
 
 # warmup the model
 predict_yaw(np.random.random([1000,3]))
